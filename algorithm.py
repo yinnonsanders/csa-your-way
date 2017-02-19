3d9102303477d9e189c8c48516ae7709b2a67485
rafasj6 <rafael.jacobovitz@gmail.com>
2017-02-18 22:34:19 -0500

    user class

--- algorithm.py
+++ algorithm.py
@@ -4,4 +4,12 @@ class Box(object):
     pass
 
 class User(object):
-    pass
+    
+    def __init__(self, id, preferences, shares):
+    """
+    input id: int that points to costumer's id
+    input preferences: list of 5 ints, 1 to 5, that correspond to user produce preferences
+    input shares: int that equals the number of shares that user owns
+    """
+    
+    
