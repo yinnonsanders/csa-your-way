33cbfa6b20658ed37715ba2e2b8ed83153c24ca4
falkjz94 <jzf5@cornell.edu>
2017-02-18 22:35:01 -0500

    box class

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,24 @@
 #algorithm for csa-your-way
 
 class Box(object):
-    pass
+    """
+    Attributes:
+    User - User associated with box (Instance of User)
+    Items - List of items in box (List of Vegetables)
+    """
+    def __init__(self, user, items):
+        self.user = user
+        self.items = []
+    
+    def add(self, item):
+        """
+        Precondition: item is of class vegetable
+        """
+        assert isinstance(item, Vegetable)
+        
+        self.items.append(item)
 
 class User(object):
     pass
+
+class Vegetable(object):
